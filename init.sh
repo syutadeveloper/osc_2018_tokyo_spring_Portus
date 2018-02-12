6b66ad1f765bd9a71a7385b22291b28b0a32a701
hashimotosyuta <syuta.hashimoto@gmail.com>
2018-02-12 15:16:00 +0900

    first commit

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# create directory for db data
+mkdir mariadb
+
+# create directory for registry volume 
+mkdir registry_volume
+
+# create directory for crt and key file
+mkdir secrets
